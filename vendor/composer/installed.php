1b8ccace3573f897111a4b7cbf89e07d7a14fc35
oiesc <emanoelescs@gmail.com>
2021-06-16 15:26:21 -0300

    init project

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '3ea26d8e994a553513b1e5caa926d98adfcc0c69',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '3ea26d8e994a553513b1e5caa926d98adfcc0c69',
+            'dev_requirement' => false,
+        ),
+    ),
+);
